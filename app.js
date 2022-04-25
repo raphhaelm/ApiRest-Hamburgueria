39002641ed2f0254e1077a50e5df41890e534012
Raphael Marques <rmarquescomputadores@gmail.com>
2022-04-25 13:39:49 -0300

    Create app.js
    Criei o servidor e Package.json

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World!")
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`
+    █████████
+    █▄█████▄█
+    █▼▼▼▼▼
+    █
+      "Servidor Online"
+    █▲▲▲▲▲
+    █████████
+     ██ ██
+    `)
+});
