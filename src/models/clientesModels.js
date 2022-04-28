8f6cc725187e3e3d5b170655e05b8b04f214ec3e
EduardoF0rtes <95946753+EduardoF0rtes@users.noreply.github.com>
2022-04-28 18:15:31 -0300

    create clienteModels

---
+++ src/models/clientesModels.js
@@ -0,0 +1,10 @@
+class clientesModels {
+    constructor(id_cliente, nome, telefone, email){
+        this.id_cliente = id_produto
+        this.nome = nome
+        this.telefone = telefone
+        this.email = email
+    }
+} 
+
+export default clientesModels;
