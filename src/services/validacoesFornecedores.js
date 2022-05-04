4aed5a63b421d8e256707f1d78d23e2525b7ffff
César Freitas <cesarfreitascontato@gmail.com>
2022-05-04 16:40:34 -0300

    fix validacoes

--- src/services/validacoesFornecedores.js
+++ src/services/validacoesFornecedores.js
@@ -1,14 +1,5 @@
 export class ValidacoesFornecedores {
     
-    // static validaInsertAndUpdate(nome, ramo, cnpj){
-    //     const cnpjNum = parseInt(cnpj)
-    //     if(nome.length >= 3 && ramo.length >= 3 && cnpj.length == 14 && cnpjNum == cnpj){
-    //         return true
-    //     } else {
-    //         return false
-    //     }
-    // }
-
     static validaNomeFornecedor(nome){
         if(nome.length >= 3){
             return true
