9b94ade142ef43ffbd72fe51949b7112c932c32e
Raphael Marques <rmarquescomputadores@gmail.com>
2022-05-04 15:44:30 -0300

    UPDATE alterei rota raiz 1

--- src/rotas/routesClientes.js
+++ src/rotas/routesClientes.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import{ inserirCliente, uptCliente, sltClientes, sltClienteId, delCliente } from "../controllers/controllersClientes.js";
 const routerClientes = Router();
 
-routerClientes.get('/', (req, res) => {
-  res.send(`Usuário Cadastrado`)
-});
+// routerClientes.get('/', (req, res) => {
+//   res.send(`Usuário Cadastrado`)
+// });
 
 routerClientes.get("/clientes", sltClientes);
 routerClientes.get("/cliente/:id", sltClienteId);
